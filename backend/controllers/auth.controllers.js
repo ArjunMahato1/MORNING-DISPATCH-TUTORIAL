512de3c3244e4c216a0787eac486994c34f87404
rupeshyadav112 <135962952+rupeshyadav112@users.noreply.github.com>
2025-02-01 15:19:15 +0530

    to choose admin account

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,102 +1,101 @@
-import bcryptjs from 'bcryptjs'
-import User from '../models/user.model.js'
-import { errorHandler } from '../utils/error.js'
-import jwt from 'jsonwebtoken'
-export const signup = async (req ,res,next) => {
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-    const { username, email, password } = req.body
-    console.log(req.body)
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
 
-    if (
-      !username ||
-      !email ||
-      !password ||
-      username === "" ||
-      email === "" ||
-      password === ""
-    ) {
-      return next(errorHandler(400, "All fields are required"))
-    }
-    const hashedPassword = bcryptjs.hashSync(password, 10)
-    const user = new User({
-      username,
-      email,
-      password: hashedPassword,
-    })
-    try {
-      await user.save()
-      res.json( "Signup Sucessful" )
-      
-    } catch (error) {
-      next(error)
-    }
-
-      
-    
-}    
-export const signin = async (req, res, next) => {
-  const{email, password}=req.body
   if (
-    
+    !username ||
     !email ||
     !password ||
-    
+    username === "" ||
     email === "" ||
     password === ""
   ) {
-    return next(errorHandler(400, "All fields are required"))
+    return next(errorHandler(400, "All fields are required"));
+  }
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const user = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  try {
+    await newUser.save();
+    res.json("Signup Sucessful");
+  } catch (error) {
+    next(error);
+  }
+};
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields are required"));
   }
   try {
-    const validUser = await User.findOne({email})
+    const validUser = await User.findOne({ email });
     if (!validUser) {
-      return next(errorHandler(404, "User not found"))
+      return next(errorHandler(404, "User not found"));
     }
-    const validPassword = bcryptjs.compareSync(password, validUser.password)
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
-      return next(errorHandler(400, "Wrong credencials"))
+      return next(errorHandler(400, "Wrong credencials"));
     }
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
-    const {password: pass, ...rest} = validUser._doc
-    res.status(200).cookie("access_token", token,{httpOnly: true}).json(rest)
-
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 export const google = async (req, res, next) => {
   const { email, name, profilePhotoUrl } = req.body;
 
   try {
-    if (!email || !name) {
-      return next(errorHandler(400, "Name and email are required"));
-    }
-
     const user = await User.findOne({ email });
+
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password: pass, ...rest } = user._doc;
-      return res.status(200)
+      return res
+        .status(200)
         .cookie("access_token", token, { httpOnly: true })
         .json(rest);
     }
 
-    const formattedName = typeof name === 'string' ? name.toLowerCase().replace(/\s+/g, '') : 'defaultuser';
     const generatedPassword =
       Math.random().toString(36).slice(-8) +
       Math.random().toString(36).slice(-8);
     const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
 
     const newUser = new User({
-      username: formattedName + Math.random().toString(9).slice(-4),
+      username:
+        name.toLowerCase().split(" ").json("") +
+        Math.random().toString(9).slice(-4),
       email,
       password: hashedPassword,
       profilePicture: profilePhotoUrl,
     });
     await newUser.save();
 
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+      { id: newUser._id, isAdmin: newUser.isAdmin },
+      process.env.JWT_SECRET
+    );
     const { password: pass, ...rest } = newUser._doc;
-    res.status(200)
+    return res
+      .status(200)
       .cookie("access_token", token, { httpOnly: true })
       .json(rest);
   } catch (error) {
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,31 +1,30 @@
-import mongoose from 'mongoose';
-const userSchema = new mongoose.Schema({
-username: {
-
-    type: String, 
-    required: true,
-    unique: true,
-
-},
-email: {
-
-type: String, 
-required: true,
-unique: true,
-},
-password: {
-
-type: String, 
-required: true,
-},
-profilePicture: {
-    type: String,
-    default: "https://cdn-icons-png.flaticon.com/128/3135/3135715.png",
-},
-
-},
- { timestamps: true}
-
-)
-const User = mongoose.model("User", userSchema)
-export default User
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default: "https://cdn-icons-png.flaticon.com/128/3135/3135715.png",
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+const User = mongoose.model("User", userSchema);
+export default User;
