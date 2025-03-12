a5e440211a1b3263272ab03d2311db6e58a1e321
rupeshyadav112 <135962952+rupeshyadav112@users.noreply.github.com>
2025-03-12 23:27:11 +0530

    Home.jsx added

--- backend/index.js
+++ backend/index.js
@@ -1,71 +1,60 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import authRoutes from './routes/auth.route.js';
-import cookieParser from 'cookie-parser';
-
-import userRoutes from './routes/user.route.js';
-
-import postRoutes from './routes/post.route.js';
-
-import commentRoutes from './routes/comment.route.js';
-
-
-
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT || 5004;   // Set port dynamically
-
+const PORT = process.env.PORT || 5004; // Set port dynamically
 // Database connection
 mongoose
   .connect(process.env.MONGO_URI, {
     ssl: true, // Enables SSL connection
   })
-  .then(() => console.log('Database is connected'))
-  .catch((err) => console.error('Database connection error:', err.message));
+  .then(() => console.log("Database is connected"))
+  .catch((err) => console.error("Database connection error:", err.message));
 
 // Middleware
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
 // Routes
-app.use('/api/auth', authRoutes)
-app.use('/api/user', userRoutes)
-app.use('/api/post', postRoutes)
-app.use('/api/comment', commentRoutes)
-
-
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
 // Error handling middleware
 app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500
-  const message = err.message || "Internal Server Error"
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
   res.status(statusCode).json({
-  success: false,
-  statusCode,
-  message,
-  })
-})
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 // Server
 const server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}!`)
+  console.log(`Server is running on port ${PORT}!`);
 });
 
 // Error handling (Optional)
-process.on('uncaughtException', (err) => {
-  console.error('Uncaught Exception:', err);
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
   server.close(() => {
-      console.log("Server shut down due to uncaught exception.");
-      process.exit(1);  // Exit the process with a failure code
+    console.log("Server shut down due to uncaught exception.");
+    process.exit(1); // Exit the process with a failure code
   });
 });
 
-process.on('SIGINT', () => {
+process.on("SIGINT", () => {
   console.log("Server is shutting down...");
   server.close(() => {
-      console.log("Server shut down successfully.");
-      process.exit(0);  // Exit the process cleanly
+    console.log("Server shut down successfully.");
+    process.exit(0); // Exit the process cleanly
   });
 });
-
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,120 @@
-import React from "react";
+import Advertise from "@/components/shared/Advertise";
+import PostCard from "@/components/shared/PostCard";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [posts, setPosts] = useState([]);
+
+  // console.log(posts)
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch("/api/post/getPosts?limit=6");
+
+      const data = await res.json();
+
+      if (res.ok) {
+        setPosts(data.posts);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <div className="flex flex-col max-w-6xl gap-6 mx-auto p-28">
+        <h1 className="text-4xl font-bold text-blue-800">
+          Welcome to <span className="text-red-600"> Morning Dispatch</span>
+        </h1>
+
+        <p className="mt-3 text-lg text-gray-600">
+          Your trusted source for the latest headlines, in-depth analysis, and
+          breaking news every morning.
+        </p>
+
+        <p className="mt-1 italic text-gray-500">Stay informed, stay ahead.</p>
+
+        <Link to={"/search"}>
+          <Button className="flex items-center gap-2 px-6 py-3 font-semibold text-black bg-yellow-400 rounded-full shadow-lg hover:bg-yellow-600 w-fit">
+            View all posts <ArrowRight className="w-5 h-5" />
+          </Button>
+        </Link>
+      </div>
+
+      <section className="pb-16 bg-white">
+        <div className="mx-auto text-center max-w-7xl">
+          <h2 className="mb-8 text-4xl font-bold text-gray-800">
+            Why You'll Love Morning Dispatch
+          </h2>
+
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
+            <FeatureCard
+              title={"Diverse Content"}
+              description={
+                "Explore news on a variety of topics, from technology to lifestyle."
+              }
+              icon="📚"
+            />
+
+            <FeatureCard
+              title={"Community Driven"}
+              description={
+                "Connect with writers and readers who share your interests."
+              }
+              icon="🌐"
+            />
+
+            <FeatureCard
+              title={"Easy to Use"}
+              description={
+                "A seamless platform for sharing and discovering great content."
+              }
+              icon="🚀"
+            />
+          </div>
+        </div>
+      </section>
+
+      <div className="p-3 bg-white">
+        <Advertise />
+      </div>
+
+      <div className="flex flex-col max-w-6xl gap-8 p-3 mx-auto py-7">
+        {posts && posts.length > 0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-bold text-slate-700">Recent Posts</h2>
+
+            <div className="flex flex-wrap gap-4">
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+
+            <Link
+              to={"/search"}
+              className="text-lg font-semibold text-center hover:underline"
+            >
+              View all news
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const FeatureCard = ({ title, description, icon }) => {
+  return (
+    <div className="p-6 text-center transition-shadow duration-300 bg-gray-100 rounded-lg shadow-md hover:shadow-lg">
+      <div className="mb-4 text-5xl">{icon}</div>
+      <h3 className="mb-2 text-2xl font-semibold text-gray-800">{title}</h3>
+      <p className="text-gray-600">{description}</p>
+    </div>
+  );
 };
 
 export default Home;
