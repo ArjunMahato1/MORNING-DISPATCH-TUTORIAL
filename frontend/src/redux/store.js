281b333ca300f21acd7c0c7d0814c65c2f4a040f
krishnakumaryadav45 <ky8702385@gmail.com>
2025-03-14 19:02:45 +0530

    mormal change

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,19 +2,15 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice'; // Corrected import
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-
 const rootReducer = combineReducers({
     user: userReducer,  // Make sure the reducer name matches
 });
-
 const persistConfig = {
     key: 'root',
     storage,
     version: 1,
 };
-
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
     reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
